efeb0e5fd02c2fe71d5847e7b870355391115b75
mhkarami97 <mhkarami97@gmail.com>
2023-12-02 08:32:27 +0330

    feat: init project

---
+++ CoreMsmq/Program.cs
@@ -0,0 +1,19 @@
+﻿using CoreMsmq;
+using MSMQ.Messaging;
+
+const int maxCount = 100;
+Console.WriteLine("start");
+
+const string queueName = @".\private$\CoreMsmq";
+Console.WriteLine($"QueueName : {queueName}");
+
+QueueMaker.CreateQueue(queueName, false);
+
+var queue = new MessageQueue(queueName);
+
+for (var i = 0; i < maxCount; i++)
+{
+    queue.Send($"test-{i}", $"label-test-{i}");
+}
+
+Console.WriteLine("finish");
---
+++ CoreMsmq/QueueMaker.cs
@@ -0,0 +1,31 @@
+﻿using MSMQ.Messaging;
+
+namespace CoreMsmq
+{
+    public static class QueueMaker
+    {
+        public static void CreateQueue(string queueName, bool useJournalQueue, bool isTransactional = false)
+        {
+            const string pathPrefix = @".\private$\";
+            var qPath = queueName;
+
+            if (!qPath.Contains(pathPrefix, StringComparison.CurrentCultureIgnoreCase))
+            {
+                qPath = pathPrefix + queueName;
+            }
+
+            if (MessageQueue.Exists(qPath))
+                return;
+
+            MessageQueue.Create(qPath, isTransactional);
+            using var messageQ = new MessageQueue(qPath);
+
+            if (!MessageQueue.Exists(qPath))
+                return;
+
+            messageQ.SetPermissions(@"Everyone", MessageQueueAccessRights.FullControl);
+            messageQ.SetPermissions(@"ANONYMOUS LOGON", MessageQueueAccessRights.FullControl);
+            messageQ.UseJournalQueue = useJournalQueue;
+        }
+    }
+}
